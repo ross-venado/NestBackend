9cbd01e46a079291c031fa34ccb62c9f27407c22
ross-venado <venado800@gmail.com>
2025-08-19 19:54:51 -0600

    hardcode port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(5000);
+
 }
 bootstrap();
