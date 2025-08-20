f57b36f296c14801607c7b437c4a0f021599ef71
ross-venado <venado800@gmail.com>
2025-08-19 19:40:38 -0600

    docker file and port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5001);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
