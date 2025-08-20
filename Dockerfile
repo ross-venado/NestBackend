e96892c0245cc4bc7005f289e0aeec3377d7b1e8
ross-venado <venado800@gmail.com>
2025-08-19 23:55:13 -0600

    Fix env loading, use ConfigModule and updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,18 @@
 # Etapa 1: Build
-FROM node:18-alpine AS builder
+FROM node:20-alpine AS builder
 
 WORKDIR /app
-
 COPY package*.json ./
 RUN npm install
-
 COPY . .
 RUN npm run build
 
 # Etapa 2: Producción
-FROM node:18-alpine
+FROM node:20-alpine
 
 WORKDIR /app
-
-COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/dist ./dist
-COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/node_modules ./node_modules
+COPY package*.json ./
 
 CMD ["node", "dist/main"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config'; // 👈 Importa ConfigModule
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/default'),
     ProductsModule,
   ],
 })
